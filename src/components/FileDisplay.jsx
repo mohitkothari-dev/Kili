b424014923403a9196bdb5f504a519d3a1ed0507
mohitkothari-dev <mrulekothari@gmail.com>
2024-02-03 15:48:17 -0500

    audio display

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const FileDisplay = ({ file, audioStream, handleAudioReset, handleFormSubmission }) => {
     const textGradient = "text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-400 to-pink-300"
+
+    const audioRef = useRef()
+
+    useEffect(() => {
+      if (!file && !audioStream) { return }   //ideal state - if nothing is uploaded
+      if (file) {
+          audioRef.current.src = URL.createObjectURL(file)
+      } else {
+          audioRef.current.src = URL.createObjectURL(audioStream)
+      }
+  }, [audioStream, file])
+
   return (
     <main className='flex-1 p-4 text-center flex flex-col justify-center gap-3 sm:gap-4 md:gap-5 max-w-full mx-auto'>
       <h1 className={`text-3xl font-bold font-mono py-3 sm:text-4xl md:text-5xl ${textGradient}`}>Your File</h1>
       <div className='my-4 flex flex-col items-center'>
         <h3 className='font-semibold'>Name</h3>
         <p>{file ? file?.name : 'Your audio'}</p>
+      </div>
+      <div className='flex flex-col mb-2'>
+        <audio ref={audioRef} className='w-full' controls>
+          Your browser does not support the audio element.
+        </audio>
       </div>
       <div className='flex items-center justify-between gap-4'>
         <button onClick={handleAudioReset} className={`${textGradient}`}>Reset</button>
